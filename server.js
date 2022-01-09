bdd5b1564ba7bd1feb134caf3aaf1fefdddf7a37
Haseeb Siddiqui <hsbsid@gmail.com>
2022-01-09 18:52:10 -0500

    Prepare for heroku deployment

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const app = express();
+const path = require('path');
 
 //db
 const connectDB = require('./config/db');
@@ -9,15 +10,20 @@ connectDB();
 app.use(express.json({ extended: false }));
 
 //routes
-app.get('/', (req, res) => {
-  res.send('Welcome to my server');
-});
-
 app.use('/api/auth', require('./routes/api/auth'));
 app.use('/api/users', require('./routes/api/users'));
 app.use('/api/posts', require('./routes/api/posts'));
 app.use('/api/boards', require('./routes/api/boards'));
 
-//start app on port 3000 (or prod server port)
+//serve static assets in production
+if (process.env.NODE_ENV === 'production') {
+  //set static folder
+  app.use(express.static('client/build'));
+  app.get('*', (re, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
+}
+
+//start app on port 3000 (or production server port)
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log('Server started on ' + PORT));
